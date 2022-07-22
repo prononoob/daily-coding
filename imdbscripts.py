22b8492106d22592d0b0e536715055b273de406b
prononoob <67364501+prononoob@users.noreply.github.com>
2022-07-22 23:30:12 +0200

    Add files via upload

--- imdbscripts.py
+++ imdbscripts.py
@@ -3,12 +3,24 @@ from bs4 import BeautifulSoup
 
 
 class Scraper():
+    global headers
+    headers = {"Accept-Language": "en-US,en;q=0.5"}
+
     def __init__(self):
         print('Script Initialized!')
         
+         
     def searchtitle(self, imID):
         properlink = 'https://www.imdb.com/title/' + imID +'/'
-        headers = {"Accept-Language": "en-US,en;q=0.5"}
         r = requests.get(properlink, headers = headers)
         soup = BeautifulSoup(r.content, 'html.parser')
         print(str(soup.title)[7:-15])
+
+    def searchrating(self, imID):
+        properlink = 'https://www.imdb.com/title/' + imID +'/'
+        r = requests.get(properlink, headers = headers)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        longtxt = soup.script
+        stars = str(longtxt).find('ratingValue')
+        print(str(longtxt)[stars+13:stars+16])
+
