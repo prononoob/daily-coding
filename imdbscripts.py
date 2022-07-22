8e62284372a5578c2ee7bf6ad873bfe1f297f7ed
prononoob <67364501+prononoob@users.noreply.github.com>
2022-07-22 19:22:28 +0200

    Update imdbscripts.py

--- imdbscripts.py
+++ imdbscripts.py
@@ -5,11 +5,10 @@ from bs4 import BeautifulSoup
 class Scraper():
     def __init__(self):
         print('Script Initialized!')
-    def searchtitle(self, imID):
         
+    def searchtitle(self, imID):
         properlink = 'https://www.imdb.com/title/' + imID +'/'
         headers = {"Accept-Language": "en-US,en;q=0.5"}
         r = requests.get(properlink, headers = headers)
         soup = BeautifulSoup(r.content, 'html.parser')
         print(str(soup.title)[7:-15])
-
