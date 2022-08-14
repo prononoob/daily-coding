ef4bf591c8c4b13b3556676dbfef59a9ae74ac3f
prononoob <lollel60606@gmail.com>
2022-08-14 17:44:38 +0200

    Working add, remove and changeStatus methods

--- notes.py
+++ notes.py
@@ -3,12 +3,19 @@
 class Notes:
     def __init__(self):
         self.items = {}
+        print('Script initialised!')
     def add(self, item):
         if item not in self.items:
             self.items[item] = False
         else:
             print(f'Item {item} already exists')
     def remove(self, item):
-        pass
+        if item in self.items:
+            self.items.pop(item)
+        else:
+            print(f'Item {item} doesn\'t exist')
     def changeState(self, item):
-        pass
+        if item in self.items:
+            self.items[item] = not self.items[item]
+        else:
+            print(f'Item {item} doesn\'t exist')
