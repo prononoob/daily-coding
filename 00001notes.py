bc5d7073b9f9083d8cd7b1f9a36561eba5503cbe
prononoob <lollel60606@gmail.com>
2022-08-14 17:00:23 +0200

    Created skeleton

--- 00001notes.py
+++ 00001notes.py
@@ -1,1 +1,12 @@
 #Notes taking program
+
+class Notes:
+    def __init__(self):
+        items = {}
+    def add(self, item):
+        items[item] = False
+    def remove(self, item):
+        pass
+    def changeState(self, item):
+        pass
+
