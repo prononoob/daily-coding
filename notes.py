76022d1e8f7a851d20e8e43b757606999b124389
prononoob <lollel60606@gmail.com>
2022-08-16 08:37:04 +0200

    Improved readability and started working on a UI

--- notes.py
+++ notes.py
@@ -1,15 +1,20 @@
-#Notes taking program
+import curses
+
+#Model
 
 class Notes:
+
     def __init__(self):
         self.items = {}
-        print('Script initialised!')
+        print('Model initialised!')
+
     def add(self, item):
         if item not in self.items:
             self.items[item] = False
         else:
             print(f'Item {item} already exists')
     def remove(self, item):
+
         if item in self.items:
             self.items.pop(item)
         else:
@@ -19,6 +24,7 @@ class Notes:
             self.items[item] = not self.items[item]
         else:
             print(f'Item {item} doesn\'t exist')
+
     def show(self):
         for item in self.items:
             if self.items[item] == True:
@@ -27,3 +33,21 @@ class Notes:
                 y = '[ ]'
             x = y + item
             print(x)
+
+#View
+
+class viewNotes:
+
+    def __init__(self):
+        stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        stdscr.keypad(True)
+        print('View initialized')
+
+    def closeView(self):
+        curses.nocbreak()
+        stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+
